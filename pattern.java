a909587e17980b47c671b2997492835832018c91
Sanika Barguje <bargujesanika02@gmail.com>
2026-03-14 12:54:02 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,1 +1,20 @@
-
+public class PatternProgram {
+  public static void main(String[] args){
+    int num = 1;
+    for(int i=1;i<=5;i++){
+      for(int j=1;j<=i;j++)
+        {
+          if(j<i)
+          {
+        System.out.print(num + " ");
+          }
+          else
+          {
+            System.out.print(num);
+          }
+          num++;
+        }
+      System.out.println();
+    }
+  }
+}
